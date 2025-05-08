b12de96a7ab40c6dc4c5ca83779b38f20588ea87
zMath3usMSF <48952393+zMath3usMSF@users.noreply.github.com>
2025-05-08 11:57:56 +0000

    update

---
+++ OperadoresRelacionais.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int numero1 = 10;
+    float numero2 = 10.5;
+
+    printf("Número 1 > Número 2: %d\n", numero1 > numero2);
+    printf("Número 1 >= Número 2: %d\n", numero1 >= numero2);
+    printf("Número 1 < Número 2: %d\n", numero1 < numero2);
+    printf("Número 1 <= Número 2: %d\n", numero1 < numero2);
+    printf("Número 1 == Número 2: %d\n", numero1 == numero2);
+    printf("Número 1 != Número 2: %d\n", numero1 != numero2);
+
+    return 0;
+}
